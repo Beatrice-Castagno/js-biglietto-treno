b7b6cd32b97e29c16f238102a51676c030bbd5fb
Beatrice Castagno <beatrice.castagno1@gmail.com>
2021-02-27 23:23:17 +0100

    Sistemate condizioni e aggiunto controllo isNaN

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,37 @@
+
+// Prendo i chilometri e l'età della persona
 var km = parseInt(prompt("Inserisci i km che devi percorrere:"));
 var eta = parseInt(prompt("Qual è la tua età?"));
 var prezzoKm = 0.21;
 
-var totBiglietto =  km * prezzoKm;
+// Cacolo valore biglietto senza sconti
+var totBiglietto = km * prezzoKm;
 
-if ( eta > 18 && eta < 65 ) {
-alert ("Il prezzo del tuo biglietto è di: " +totBiglietto+ "€. Sconto non applicabile");
-}
-
+// Calcolo gli sconti
 var sconto20 = totBiglietto * 20 / 100;
 var totBigliettoSconto20 = totBiglietto - sconto20;
 var sconto40 = totBiglietto * 40 / 100;
 var totBigliettoSconto40 = totBiglietto - sconto40;
 
-if ( eta < 18 ) {
-  alert("Il prezzo del tuo biglietto è di: " +totBigliettoSconto20+ "€. Sconto 20%");
-} else if ( eta > 65 && eta <= 100) {
- alert("Il prezzo del tuo biglietto è di: " +totBigliettoSconto40+ "€. Sconto 40%");
-} else {
- alert ("I dati che hai inserito non sono corretti. Riprova.");
+// Questo alert si attiva nel caso in cui l'età o i km non siano numeri
+if (isNaN(eta) || isNaN(km)) {
+	alert("I dati che hai inserito non sono corretti. Riprova.");
+}
+
+/* Tipi di sconti applicabili:
+*
+* - prezzo normale
+* - 20% di sconto
+* - 40% di sconto
+*
+*/
+
+else {
+	if (eta <= 18) {
+		alert("Il prezzo del tuo biglietto è di: " + totBigliettoSconto20 + "€. Sconto 20%");
+	} else if (eta >= 65) {
+		alert("Il prezzo del tuo biglietto è di: " + totBigliettoSconto40 + "€. Sconto 40%");
+	} else {
+		alert("Il prezzo del tuo biglietto è di: " + totBiglietto + "€. Sconto non applicabile");
+	}
 }
